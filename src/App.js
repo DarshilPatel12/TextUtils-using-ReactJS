383ef74ff4f6dd00a4e629556ccaf2498c46788c
DarshilPatel12 <99804230+DarshilPatel12@users.noreply.github.com>
2022-08-06 16:50:25 +0530

    TextUtils-using-ReatcJS

---
+++ src/App.js
@@ -0,0 +1,56 @@
+
+import React, { useState } from 'react';
+import './App.css';
+import Alert from './components/Alert';
+// import About from './components/About';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+// import {
+//   BrowserRouter as Router,
+//   Route,
+//   Routes
+// } from "react-router-dom";
+
+function App() {
+  const [mode , setMode] = useState('light');
+  const [alert , setAlert] = useState(null);
+
+  const showAlert = (message, type)=>{
+      setAlert({
+        msg: message,
+        type: type
+      })
+      setTimeout(() => {
+        setAlert(null);
+      }, 1500);
+  }
+  const toggleMode = ()=>{
+    if(mode === 'light'){
+      setMode('dark');
+      document.body.style.backgroundColor = '#042743';
+      showAlert("Dark mode is been enabled", "success");
+    }
+    else{
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+      showAlert("Light mode is been enabled", "success");
+    }
+  }
+  return (
+    <> 
+    {/* <Router> */}
+    <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode}/>
+    <Alert alert={alert}/>
+    <div className="container my-3">
+    {/* <Routes> */}
+        {/* <Route exact path="/about" element={<About mode={mode} />} /> */}
+        {/* <Route exact path="/" element={<TextForm showAlert={showAlert} heading="Try TextUtils - Word Counter, Character Counter, Remove extra Spaces" mode={mode}/>} /> */}
+        <TextForm showAlert={showAlert} heading="Try TextUtils - Word Counter, Character Counter, Remove extra Spaces" mode={mode}/>
+    {/* </Routes> */}
+    </div>
+    {/* </Router> */}
+    </>
+  );
+}
+
+export default App;
